e5832795b596b8ad6878f53ac9d0dd370c156d00
tao.jing <wuxianshen@163.com>
2019-03-14 20:02:54 +0800

    Initial version of land april tag demo

---
+++ inc/vision_defs.h
@@ -0,0 +1,18 @@
+namespace vision_defs
+{
+#pragma push(1)
+	typedef struct _land_mark_pos
+	{
+		int mark_id;
+		int hammingDistance;
+		double distance;
+		double x;
+		double y;
+		double z;
+		double yaw;
+		double pitch;
+		double roll;
+
+	}land_mark_pos;
+#pragma pop()
+};
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "AprilTag_TX2.h"
+
+int main(int argc, char* argv[])
+{
+	AprilTag demo;
+	cv::Mat image;
+	// process command line options
+	demo.parseOptions(argc, argv);
+	demo.setup();
+	cout << "Processing image" << endl;
+	cv::VideoCapture m_cap("../../res/test.mp4");
+	//cv::VideoCapture m_cap(1);
+	int frame = 0;
+	while (true) { 
+		// capture frame
+		m_cap >> image;
+		demo.loadImages(image);
+		if (cv::waitKey(1) >= 0) break;
+		frame++;
+		cout << demo.pos_vec.size() << " tags detected:" << endl;
+		for (vector<vision_defs::land_mark_pos>::size_type i = 0; i != demo.pos_vec.size(); i ++)
+		{
+			cout << "Id=" << demo.pos_vec[i].mark_id
+				<< " distance=" << demo.pos_vec[i].distance
+				<< "m,x=" << demo.pos_vec[i].x
+				<< ", y=" << demo.pos_vec[i].y
+				<< ", z=" << demo.pos_vec[i].z
+				<< ", yaw=" << demo.pos_vec[i].yaw
+				<< ", pitch=" << demo.pos_vec[i].pitch
+				<< ", roll=" << demo.pos_vec[i].roll
+				<< endl;
+		}
+	}
+
+	return 0;
+}
